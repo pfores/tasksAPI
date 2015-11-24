7b540520aa217d064d83d4a2e9d55e5ab28bae76
vagrant <vagrant@homestead>
2015-11-24 20:53:45 +0000

    Tag i Task correcte, funcionen

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Task;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // forma de no fer API correctament
+        // 1. No es retorna paginació
+        return Task::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $task = new Task();
+
+        $this->saveTask($request, $task);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Task::findOrFail($id);
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $task = Task::findOrFail($id);
+
+        $this->saveTask($request, $task);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        Task::destroy($id);
+    }
+
+    /**
+     * @param Request $request
+     * @param $task
+     */
+    protected function saveTask(Request $request, $task)
+    {
+        $task->name = $request->name;
+        $task->priority = $request->priority;
+        $task->done = $request->done;
+
+        $task->save();
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Tag;
+use App\Task;
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 
@@ -16,6 +18,29 @@ class DatabaseSeeder extends Seeder
 
         // $this->call(UserTableSeeder::class);
 
+        $faker = Faker\Factory::create();
+        $this->seedTasks($faker);
+        $this->seedTags($faker);
+
         Model::reguard();
+    }
+
+    private function seedTasks($faker) {
+        foreach (range(0,100) as $number) {
+            $task= new Task();
+
+            $task->name = $faker->sentence;
+            $task->done = $faker->boolean;
+            $task->priority = $faker->randomDigit;
+            $task->save();
+        }
+    }
+
+    private function seedTags($faker) {
+        foreach (range(0,100) as $item) {
+            $tag= new Tag();
+            $tag->name = $faker->word;
+            $tag->save();
+        }
     }
 }
